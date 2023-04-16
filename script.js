9fe70e9a9f747e8ebf6b21e9998cc1a14af3f92c
Kauan Lima <kauanalmeidalima1405@gmail.com>
2023-04-16 13:54:24 -0300

    Correção de erros

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let situacaoFinalID = document.querySelector("#situacaofinal");
 
 function calcularNotas() {
   try {
-    nota1 = parseFloat(document.getElementById("nota1").value);
-    nota2 = parseFloat(document.getElementById("nota2").value);
-    notaAp = parseFloat(document.getElementById("nota3").value);
+    nota1 = parseFloat(document.getElementById("nota1").value.replace(",", "."));
+    nota2 = parseFloat(document.getElementById("nota2").value.replace(",", "."));
+    notaAp = parseFloat(document.getElementById("nota3").value.replace(",", "."));
     if (nota1 > 3 || nota2 > 3 || notaAp > 4) {
       throw Error("Digite um valor dentro da nota máxima");
     }
@@ -20,16 +20,16 @@ function calcularNotas() {
     const somaDasAtv = nota1 + nota2 + notaAp;
 
     if (somaDasAtv >= 7) {
-      notaFinalID.innerHTML = somaDasAtv;
+      notaFinalID.innerHTML = somaDasAtv.toFixed(2).replace(".", ",");
       situacaoFinalID.innerHTML = "Aprovado";
     } else {
       if (somaDasAtv >= 6.5 && somaDasAtv <= 6.9) {
         alert("Alunos com Nota Final 6,5 a 6,9 terão NF arredondada para 7!")
-        notaFinalID.innerHTML = "7";
+        notaFinalID.innerHTML = "7,00";
         situacaoFinalID.innerHTML = "Aprovado";
       } else {
         provaInstitucional = parseFloat(
-          prompt("Digite sua nota da Prova Institucional: ")
+          prompt("Digite sua nota da Prova Institucional: ").replace(",", ".")
         );
 
         if (provaInstitucional > 10 || isNaN(provaInstitucional)) {
@@ -39,7 +39,7 @@ function calcularNotas() {
         }
 
         const notaFinal = (somaDasAtv + provaInstitucional) / 2;
-        notaFinalID.innerHTML = notaFinal;
+        notaFinalID.innerHTML = notaFinal.toFixed(2).replace(".", ",");
 
         if (notaFinal >= 5) {
           situacaoFinalID.innerHTML = "Aprovado";
@@ -50,12 +50,11 @@ function calcularNotas() {
     }
   } catch (error) {
     alert(error.message);
-    refazerCode()
-  
+    refazerCode();
   }
 }
 
 function refazerCode() {
-  document.querySelector("#situacaofinal").innerHTML = ""
-  document.querySelector("#notafinal").innerHTML = ""
+  document.querySelector("#situacaofinal").innerHTML = "";
+  document.querySelector("#notafinal").innerHTML = "";
 }
